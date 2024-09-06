3963e3bc58e81538705e366fcbdca24e7c841c56
ressharu <haruki20050202@icloud.com>
2024-09-06 15:36:35 +0900

    無駄なコメントを削除

--- WeatherForecast/WeatherForecast/Services/YumemiWeatherAPIService.swift
+++ WeatherForecast/WeatherForecast/Services/YumemiWeatherAPIService.swift
@@ -12,19 +12,12 @@ final class YumemiWeatherAPIService {
     // リロード用の新しいメソッドを定義
     static func reloadWeather(for area: String, completion: @escaping (Result<WeatherCondition, Error>) -> Void) {
         do {
-            // fetchWeatherCondition(at:) を呼び出す
             let weatherConditionString = try YumemiWeather.fetchWeatherCondition(at: area)
-            
-            // 天気の状態を WeatherCondition に変換
             let weatherCondition = WeatherCondition(rawValue: weatherConditionString) ?? .sunny
-            
-            // 成功時に結果を completion に渡す
             completion(.success(weatherCondition))
         } catch let error as YumemiWeatherError {
-            // YumemiWeatherError 型のエラーをキャッチ
             completion(.failure(error))
         } catch {
-            // その他の予期しないエラーをキャッチ
             completion(.failure(error))
         }
     }
