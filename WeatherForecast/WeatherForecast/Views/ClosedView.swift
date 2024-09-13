d742dad2d94a93c52a25d0505d38739d1b7cb0b8
ressharu <haruki20050202@icloud.com>
2024-09-13 13:09:09 +0900

    protcolを定義したことに対応

---
+++ WeatherForecast/WeatherForecast/Views/ClosedView.swift
@@ -0,0 +1,23 @@
+//
+//  ClosedView.swift
+//  WeatherForecast
+//
+//  Created by 渡邉 華輝 on 2024/09/10.
+//
+
+import SwiftUI
+
+struct ClosedView: View {
+    @State private var isPresented = false
+    
+    var body: some View {
+        Color.black
+            .background(.black)
+            .task(id: isPresented) {
+                isPresented = true
+            }
+            .fullScreenCover(isPresented: $isPresented) {
+                ForecastView(weatherController: WeatherControllerImpl())
+            }
+    }
+}
